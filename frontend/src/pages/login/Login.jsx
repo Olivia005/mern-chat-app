d13fbd9ad3c7d859c6647abb64f92611e834d7f2
Olivia Sikder <oliviasikder@example.com>
2024-11-12 17:05:49 +0530

    added welcome update

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
 	return (
 		
 		<div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
-			<p class="text-lg...">Welcome to Sleipner</p>
+			<p class="text-2xl text-white">Welcome to Sleipner</p>
 			<p class="text-base ...">We're excited to have you here. Feel free to use it!</p>
 			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
 				<h1 className='text-3xl font-semibold text-center text-gray-300'>
